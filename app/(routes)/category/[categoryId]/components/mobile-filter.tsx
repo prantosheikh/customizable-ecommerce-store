ef7bee6f4fade5957ad2533155e53323bbfb07f8
Pranto sheikh <prantosheikh834@gmail.com>
2024-03-25 19:49:22 +0600

    final commit

--- app/(routes)/category/[categoryId]/components/mobile-filter.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filter.tsx
@@ -1,65 +1,74 @@
 "use client";
 
-import Button from "@/components/ui/Button";
-import IconButton from "@/components/ui/icon-button";
-import { Color, Size } from "@/types";
-import { Dialog } from "@headlessui/react";
+import { useState } from "react";
 import { Plus, X } from "lucide-react";
-import React, { useState } from "react";
-import Filter from "./filter";
+import { Dialog } from "@headlessui/react";
 
-interface MobileFilterProps {
-   sizes: Size[];
-   colors: Color[];
+import IconButton from "@/components/ui/icon-button";
+// import Button from "@/components/ui/button";
+import { Color, Size } from "@/types";
+
+import Filter from "./filter";
+import Button from "@/components/ui/Button";
+
+interface MobileFiltersProps {
+	sizes: Size[];
+	colors: Color[];
 }
 
-const MobileFilter: React.FC<MobileFilterProps> = ({ sizes, colors }) => {
-   const [open, setOpen] = useState(false);
+const MobileFilters: React.FC<MobileFiltersProps> = ({ sizes, colors }) => {
+	const [open, setOpen] = useState(false);
 
-   const onOpen = () => setOpen(true);
-   const onClose = () => setOpen(false);
+	const onOpen = () => setOpen(true);
+	const onClose = () => setOpen(false);
 
-   return (
-      <div>
-         <Button
-            onClick={onOpen}
-            className="flex items-center gap-x-2 lg:hidden"
-         >
-            Filter
-            <Plus size={20} />
-         </Button>
+	return (
+		<>
+			<Button
+				onClick={onOpen}
+				className="flex items-center gap-x-2 lg:hidden"
+			>
+				Filters
+				<Plus size={20} />
+			</Button>
 
-         <Dialog
-            open={open}
-            as="div"
-            className="relative z-40 lg:hidden"
-            onClose={onClose}
-         >
-            <div className="fixed inset-0 bg-black bg-opacity-25 ">
-               <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-auto bg-white py-4 pb-6 shadow-xl ">
-                  {/* ad */}
-                  <div className="flex items-center justify-end px-4">
-                     <IconButton icon={<X size={15} />} onClick={onClose} />
-                  </div>
-                  {/* Reander the filter */}
-                  <div className="p-4">
-                     <Filter
-                        valueKey="sizeId"
-                        name="Sizes"
-                        data={sizes}
-                     />
-                     <Filter
-                        valueKey="colorId"
-                        name="Colors"
-                        data={colors}
-                     />
-                  </div>
+			<Dialog
+				open={open}
+				as="div"
+				className="relative z-40 lg:hidden"
+				onClose={onClose}
+			>
+				{/* Background color and opacity */}
+				<div className="fixed inset-0 bg-black bg-opacity-25" />
 
-               </Dialog.Panel>
-            </div>
-         </Dialog>
-      </div>
-   );
+				{/* Dialog position */}
+				<div className="fixed inset-0 z-40 flex">
+					<Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+						{/* Close button */}
+						<div className="flex items-center justify-end px-4">
+							<IconButton
+								icon={<X size={15} />}
+								onClick={onClose}
+							/>
+						</div>
+
+						<div className="p-4">
+							<Filter
+								valueKey="sizeId"
+								name="Sizes"
+								data={sizes}
+							/>
+							<Filter
+								valueKey="colorId"
+								name="Colors"
+								data={colors}
+							/>
+						</div>
+					</Dialog.Panel>
+				</div>
+			</Dialog>
+		</>
+	);
 };
 
-export default MobileFilter;
+export default MobileFilters;
