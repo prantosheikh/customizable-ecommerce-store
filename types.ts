ca18895259bcd127e78886c22de2e795feb23fc8
Pranto sheikh <prantosheikh834@gmail.com>
2024-03-26 15:44:23 +0600

    a

--- types.ts
+++ types.ts
@@ -1,3 +1,19 @@
+export interface Product {
+	id: string;
+	category: Category;
+	name: string;
+	price: string;
+	isFeatured: boolean;
+	size: Size;
+	color: Color;
+	images: Image[];
+}
+
+export interface Image {
+	id: string;
+	url: string;
+}
+
 export interface Billboard {
 	id: string;
 	label: string;
@@ -10,27 +26,12 @@ export interface Category {
 	billboard: Billboard;
 }
 
-export interface Product {
-	id: string;
-	category: Category;
-	name: string;
-	price: string;
-	isFeatured: boolean;
-	size: string;
-	color: Color;
-	images: Image[];
-}
-
-export interface Image {
-	id: string;
-	url: string;
-}
-
 export interface Size {
 	id: string;
 	name: string;
 	value: string;
 }
+
 export interface Color {
 	id: string;
 	name: string;
