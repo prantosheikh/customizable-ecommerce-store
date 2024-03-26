6fbb54bf5315bf690d91d7496a648d51203481e4
Pranto sheikh <prantosheikh834@gmail.com>
2024-03-26 15:55:53 +0600

    adb

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,15 +1,18 @@
-import getCategory from "@/actions/get-category";
-import getColors from "@/actions/get-colors";
-import getProducts from "@/actions/get-products";
-import getSizes from "@/actions/get-sizes";
-import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
-import React from "react";
-import Filter from "./components/filter";
-
-import NoResult from "@/components/no-result";
+// import Billboard from "@/components/ui/billboard";
 import ProductCard from "@/components/ui/product-card";
-import MobileFilter from "./components/mobile-filter";
+// import NoResults from "@/components/ui/no-results";
+
+import getProducts from "@/actions/get-products";
+import getCategory from "@/actions/get-category";
+import getSizes from "@/actions/get-sizes";
+import getColors from "@/actions/get-colors";
+
+import Filter from "./components/filter";
+import Billboard from "@/components/billboard";
+import MobileFilters from "./components/mobile-filter";
+import NoResult from "@/components/no-result";
+// import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -31,10 +34,8 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
 		categoryId: params.categoryId,
 		colorId: searchParams.colorId,
 		sizeId: searchParams.sizeId,
+		isFeatured: true,
 	});
-
-	// console.log(products);
-
 	const sizes = await getSizes();
 	const colors = await getColors();
 	const category = await getCategory(params.categoryId);
@@ -45,7 +46,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
 				<Billboard data={category.billboard} />
 				<div className="px-4 sm:px-6 lg:px-8 pb-24">
 					<div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-						<MobileFilter sizes={sizes} colors={colors} />
+						<MobileFilters sizes={sizes} colors={colors} />
 						<div className="hidden lg:block">
 							<Filter
 								valueKey="sizeId"
@@ -58,9 +59,9 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
 								data={colors}
 							/>
 						</div>
-						<div className="mt-6 lg:col-span-4 lg:mt-0 ">
+						<div className="mt-6 lg:col-span-4 lg:mt-0">
 							{products.length === 0 && <NoResult />}
-							<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+							<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
 								{products.map((item) => (
 									<ProductCard key={item.id} data={item} />
 								))}
